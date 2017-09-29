6ffd771898699fffc20b161b1b1224970f45ef02
alekzonder <alekzonder@gmail.com>
2017-09-29 18:10:22 +0700

    chg: disable prefer-destructuring

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
         indent: ['error', 4],
         'comma-dangle': ['error', 'never'],
         'no-underscore-dangle': ['error', { allowAfterThis: true }],
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'prefer-destructuring': [0]
     }
 };
