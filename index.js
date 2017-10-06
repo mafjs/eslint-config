9bd2f640fd9bacb8c9d19e83efd99c8c2b09c0b6
alekzonder <alekzonder@gmail.com>
2017-10-06 14:01:36 +0700

    feat: move from airbnb to google codestyle

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 module.exports = {
-    extends: 'airbnb-base',
-
-    rules: {
-        indent: ['error', 4],
-        'comma-dangle': ['error', 'never'],
-        'no-underscore-dangle': ['error', { allowAfterThis: true }],
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-        'prefer-destructuring': [0],
-        'newline-per-chained-call': ['error', { ignoreChainWithDepth: 9 }]
-    }
+    extends: ["eslint:recommended", "google"],
+    parser: "babel-eslint"
 };
