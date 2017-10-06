e820c29748b040596e1ac07950a5c11ebdd477ce
alekzonder <alekzonder@gmail.com>
2017-10-06 19:32:31 +0700

    chg: max-len = 100

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ module.exports = {
     parser: "babel-eslint",
     globals: {
         Promise: false
+    },
+    rules: {
+        'max-len': [2, {
+              code: 100,
+              tabWidth: 4,
+              ignoreUrls: true,
+              ignorePattern: '^goog\.(module|require)',
+            }],
     }
 };
