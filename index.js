c6c3dce6386845cfd58a5713b84445de444c3da0
alekzonder <alekzonder@gmail.com>
2017-10-14 09:19:12 +0700

    feat: comma-dangle never, add default env node:true

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 module.exports = {
-    extends: ["eslint:recommended", "google"],
-    parser: "babel-eslint",
+    extends: ['eslint:recommended', 'google'],
+    parser: 'babel-eslint',
     globals: {
         Promise: false
+    },
+    env: {
+        node: true
     },
     rules: {
         'max-len': [2, {
               code: 100,
               tabWidth: 4,
               ignoreUrls: true,
-              ignorePattern: '^goog\.(module|require)',
+              ignorePattern: '^goog.(module|require)'
             }],
+        'comma-dangle': ['error', 'never']
     }
 };
